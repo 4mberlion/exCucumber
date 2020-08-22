b02eac09311a714987091aa988208e0a78ab9e62
Maestro3733 <extremewinds@gmail.com>
2020-08-23 01:16:14 +0200

    Initial commit

---
+++ src/test/java/LoginStep.java
@@ -0,0 +1,22 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class LoginStep {
+    @And("I enter the username as admin and password as admin")
+    public void iEnterTheUsernameAsAdminAndPasswordAsAdmin() {
+         
+    }
+
+    @Given("I navigate to the login page")
+    public void iNavigateToTheLoginPage() {
+    }
+
+    @And("I click login button")
+    public void iClickLoginButton() {
+    }
+
+    @Then("I should see the user form page")
+    public void iShouldSeeTheUserFormPage() {
+    }
+}
